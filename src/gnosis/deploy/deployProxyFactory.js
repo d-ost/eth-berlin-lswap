2642ca63dc2da1fe213bde0c7acadc62ea72023e
Puneet Khushwani <puneet.khushwani@ost.com>
2019-08-25 01:04:40 +0200

    deployment scripts for gnosis contracts + code to transfer / swap tokens out of gnosis safe contract

---
+++ src/gnosis/deploy/deployProxyFactory.js
@@ -0,0 +1,31 @@
+const Web3 = require('web3');
+const OpenST = require('@openst/openst.js');
+
+async function deploy() {
+  const web3 = new Web3('https://mosaicdao.org/aux/1405');
+  // const web3 = new Web3('https://mosaicdao.org/origin/goerli');
+  const privateKey = '';
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+  console.log('address ', account.address);
+  web3.eth.accounts.wallet.add(account);
+
+
+  txOptions = {
+    from: account.address,
+    gasPrice: '0x3B9ACA00',
+    gas: '7050328'
+  };
+
+  const userSetup = new OpenST.Setup.User(web3);
+  let proxyFactoryAddress;
+  userSetup.deployProxyFactory(txOptions).then(function(response){
+    proxyFactoryAddress = response.receipt.contractAddress;
+    console.log('proxyFactoryAddress', proxyFactoryAddress);
+  });
+}
+
+deploy()
+  .then(console.log)
+  .catch((error) => {
+  console.log('Big error ', error);
+});
