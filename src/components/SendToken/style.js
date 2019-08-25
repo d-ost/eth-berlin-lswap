26ee5faee37c4abb8eeeb145c96eb3358a65ff7a
Mayur Patil <mayurupatil@gmail.com>
2019-08-25 03:53:21 +0200

    fix for homescreen

---
+++ src/components/SendToken/style.js
@@ -0,0 +1,20 @@
+export default {
+    homeContainer: {            
+        height: '100%',
+        backgroundColor:'#fff',
+        color: '#fff'
+    },
+
+    header: {
+        height: '56px',
+        backgroundColor: '#fc5468',
+        boxShadow: 'rgba(0, 0, 0, 0.25) 0px 5px 15px'
+    },
+
+    headerText: {
+        color: '#fff',
+        fontWeight: 'bolder',
+        fontSize: '24px'
+    }    
+}
+
