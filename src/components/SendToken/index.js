cb2d1ae1de6e1aceb6502c281328f6cdb9c3f517
Mayur Patil <mayurupatil@gmail.com>
2019-08-25 04:58:44 +0200

    send

--- src/components/SendToken/index.js
+++ src/components/SendToken/index.js
@@ -5,12 +5,14 @@ import QrReader from 'react-qr-reader';
 import coreConstants from '../../config/coreConstants';
 import GetSendOptions from '../../lib/GetSendOptions';
 import ls from '../../lib/localStorage';
+import Send from '../../lib/Send';
 
 class SendToken extends Component {
   constructor(props) {
     super(props);
     this.state = {qrcodeValue: '', amountToSend: 0, sendScreen: false};
     this.AuxBurnerKey = null;
+    this.checkedSteps = [];
   }
 
   handleError = () => {};
@@ -31,15 +33,12 @@ class SendToken extends Component {
   };
 
   sendAmount = async () => {
-   
     let senderOriginAddress = JSON.parse(
       await ls.getItem(coreConstants.ORIGIN_BURNER_KEY),
     ).address;
     let senderAuxAddress =
       (await ls.getItem(coreConstants.AUX_BURNER_KEY)) &&
       JSON.parse(await ls.getItem(coreConstants.AUX_BURNER_KEY)).address;
-
-   
 
     let senderObj = {
       sendTokenName: this.props.currentSelectedSend.tokenName,
@@ -59,30 +58,61 @@ class SendToken extends Component {
     if (this.sendConfig.success) {
       this.setState({sendScreen: true});
     }
-    console.log(this.sendConfig);
   };
 
-  showSendScreen = () => {
+  showSendScreen() {
     let data = this.sendConfig.data;
-    return data.stepsConfig.map(config => {
+    let mapped = data.stepsConfig.map(config => {
       let val = Object.values(config)[0];
-      return (<div className="custom-control custom-checkbox m-3" style={{color: '#4d5d71'}}>
-      <input type="checkbox" class="custom-control-input" id="customCheck" name={Object.keys(config)[0]} defaultChecked={true}  onChange={this.handleInputChange} />
-      <label class="custom-control-label" for="customCheck">{val.msg}</label>
-    </div>);
+      this.checkedSteps.push(Object.keys(config)[0]);
+      return (
+        <div
+          className="custom-control custom-checkbox m-3"
+          style={{color: '#4d5d71'}}>
+          <input
+            type="checkbox"
+            class="custom-control-input"
+            id="customCheck"
+            name={Object.keys(config)[0]}
+            defaultChecked={true}
+            onChange={this.handleInputChange}
+          />
+          <label class="custom-control-label" htmlFor="customCheck">
+            {val.msg}
+          </label>
+        </div>
+      );
     });
-  };
 
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value =  target.checked;
-    const name = target.name;
-    
-    this.setState({
-      [name]: value
-    });
+    return (
+      <div>
+        {mapped}
+        <div class="row">
+          <button
+            className="btn btn-primary my-4 mx-auto"
+            style={{
+              backgroundColor: 'rgb(52, 68, 91)',
+              borderColor: '#4e5d71',
+            }}
+            onClick={this.finalSend}>
+            Send
+          </button>
+        </div>
+      </div>
+    );
   }
+
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.checked;
+    const name = target.name;
+    var index = this.checkedSteps.indexOf(name);
+    if (!value) {
+      if (index !== -1) this.checkedSteps.splice(index, 1);
+    } else {
+      if (index < 0) this.checkedSteps.push(name);
+    }
+  };
 
   finalSend = async () => {
     let senderOriginAddress = JSON.parse(
@@ -91,8 +121,6 @@ class SendToken extends Component {
     let senderAuxAddress =
       (await ls.getItem(coreConstants.AUX_BURNER_KEY)) &&
       JSON.parse(await ls.getItem(coreConstants.AUX_BURNER_KEY)).address;
-
-   
 
     let senderObj = {
       sendTokenName: this.props.currentSelectedSend.tokenName,
@@ -104,9 +132,77 @@ class SendToken extends Component {
         [this.props.currentSelectedSend.tokenName]: this.props
           .currentSelectedSend.tokenInfo,
       },
+      steps: this.checkedSteps,
+      senderOriginPrivateKey: JSON.parse(
+        await ls.getItem(coreConstants.ORIGIN_BURNER_KEY),
+      ).privateKey,
     };
 
-  }
+    let send = new Send(senderObj),
+    response = await send.perform();
+    console.log(response, 'senderObjsenderObjsenderObjsenderObj');    
+  };
+
+  showSendScreenHandler = () => {
+    return this.state.sendScreen
+      ? this.showSendScreen()
+      : this.showFirstScreens();
+  };
+
+  showFirstScreens = () => {
+    return this.state.qrcodeValue ? (
+      <div>
+        <div style={{backgroundColor: '#fff', color: '#4d5d71'}}>
+          <div class="form-group m-3">
+            <label htmlFor="formGroupExampleInput">Amount</label>
+            <input
+              type="number"
+              class="form-control"
+              id="formGroupExampleInput"
+              placeholder="Enter amount here"
+              value={this.state.amountToSend}
+              onChange={this.changeAmount}
+            />
+          </div>
+          <div class="row">
+            <button
+              className="btn btn-primary my-4 mx-auto"
+              style={{
+                backgroundColor: 'rgb(52, 68, 91)',
+                borderColor: '#4e5d71',
+              }}
+              onClick={this.sendAmount}>
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div>
+        <div className="row justify-content-center my-4">
+          {/* <QR style={{width: '500px', height: '500px'  }} className='align-self-center' value={this.state.qrcodeValue}/> */}
+
+          <QrReader
+            delay={300}
+            onError={this.handleError}
+            onScan={this.handleScan}
+            style={{width: '300px', height: '300px'}}
+          />
+        </div>
+        <div class="row">
+          <button
+            className="btn btn-primary my-4 mx-auto"
+            style={{
+              backgroundColor: 'rgb(52, 68, 91)',
+              borderColor: '#4e5d71',
+            }}
+            onClick={this.props.onHide}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    );
+  };
 
   render() {
     return (
@@ -116,71 +212,7 @@ class SendToken extends Component {
             Send
           </div>
         </div>
-        {this.state.sendScreen ? (
-          this.showSendScreen()
-          (<div class="row">
-          <button
-            className="btn btn-primary my-4 mx-auto"
-            style={{
-              backgroundColor: 'rgb(52, 68, 91)',
-              borderColor: '#4e5d71',
-            }}
-            onClick={this.finalSend}>
-            Send
-          </button>
-        </div>)
-        ) : this.state.qrcodeValue ? (
-          <div>
-            <div style={{backgroundColor: '#fff', color: '#4d5d71'}}>
-              <div class="form-group m-3">
-                <label htmlFor="formGroupExampleInput">Amount</label>
-                <input
-                  type="number"
-                  class="form-control"
-                  id="formGroupExampleInput"
-                  placeholder="Enter amount here"
-                  value={this.state.amountToSend}
-                  onChange={this.changeAmount}
-                />
-              </div>
-              <div class="row">
-                <button
-                  className="btn btn-primary my-4 mx-auto"
-                  style={{
-                    backgroundColor: 'rgb(52, 68, 91)',
-                    borderColor: '#4e5d71',
-                  }}
-                  onClick={this.sendAmount}>
-                  Send
-                </button>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div>
-            <div className="row justify-content-center my-4">
-              {/* <QR style={{width: '500px', height: '500px'  }} className='align-self-center' value={this.state.qrcodeValue}/> */}
-
-              <QrReader
-                delay={300}
-                onError={this.handleError}
-                onScan={this.handleScan}
-                style={{width: '300px', height: '300px'}}
-              />
-            </div>
-            <div class="row">
-              <button
-                className="btn btn-primary my-4 mx-auto"
-                style={{
-                  backgroundColor: 'rgb(52, 68, 91)',
-                  borderColor: '#4e5d71',
-                }}
-                onClick={this.props.onHide}>
-                Cancel
-              </button>
-            </div>
-          </div>
-        )}
+        {this.showSendScreenHandler()}
       </div>
     );
   }
